7042b314e7ca1e74d41b7dc616fe9b03d10a44ed
Tamojit Das <tamojitdas181007@gmail.com>
2024-11-26 13:53:32 +0530

    Assets, fonts & tailwind configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-3xl text-red-500 font-bold underline">
+      <h1 className="text-3xl text-violet-300 font-bold underline font-robert-medium">
         Hello world!
       </h1>
     </main>
