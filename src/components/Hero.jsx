8ffaec5f3745c74dc95cf8928c61605508789d16
Tamojit Das <tamojitdas181007@gmail.com>
2024-11-26 21:12:20 +0530

    Hero section animations using GSAP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 /* eslint-disable no-unused-vars */
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useState } from "react"
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+// registering scroll-trigger animations of GSAP
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
 	const [currentIndex, setCurrentIndex] = useState(1);
@@ -10,7 +16,7 @@ const Hero = () => {
 	const [isLoading, setIsLoading] = useState(true);
 	const [loadedVideos, setLoadedVideos] = useState(0);
 
-	const totalVideos = 3;
+	const totalVideos = 4;
 	const nextVideoRef = useRef(null);
 	const upComingVideoIndex = (currentIndex % totalVideos) + 1; //circular rendering/indexing
 
@@ -25,8 +31,68 @@ const Hero = () => {
 		setLoadedVideos((prev) => prev + 1);
 	}
 
+	useEffect(() => {
+		if (loadedVideos === totalVideos - 1) {
+			setIsLoading(false);
+		}
+	}, [loadedVideos]);
+
+	// Animations using GSAP Plugin
+	useGSAP(() => {
+		if (hasClicked) {
+			gsap.set('#next-video', { visibility: "visible" });
+
+			//animator configs func.
+			gsap.to("#next-video", {
+				transformOrigin: 'center center',
+				scale: 1,
+				width: '100%',
+				height: '100%',
+				duration: 1,
+				ease: 'power1.inOut',
+				onStart: () => nextVideoRef.current.play(),
+			});
+
+			gsap.from("#current-video", {
+				transformOrigin: 'center center',
+				scale: 0,
+				duration: 1.5,
+				ease: 'power1.inOut'
+			})
+		}
+	}, { dependencies: [currentIndex], revertOnUpdate: true });
+
+	useGSAP(() => {
+		gsap.set("#video-frame", {
+			clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+			borderRadius: '0 0 40% 10%'
+		});
+
+		gsap.from('#video-frame', {
+			clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+			borderRadius: '0 0 0 0',
+			ease: 'power1.inOut',
+			scrollTrigger: {
+				trigger: '#video-frame',
+				start: 'center center',
+				end: 'bottom center',
+				scrub: true
+			}
+		});
+	});
+
 	return (
 		<div className="relative h-dvh w-screen overflow-x-hidden">
+			{isLoading && (
+				<div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+					<div className="three-body">
+						<div className="three-body__dot" />
+						<div className="three-body__dot" />
+						<div className="three-body__dot" />
+					</div>
+				</div>
+			)}
+
 			<div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
 				<div>
 					<div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
